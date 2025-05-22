8f7e21c0e8d47f7e5e5aa7a1b4275a5f83a62794
Marvin <marvinbarretto@gmail.com>
2025-05-22 20:03:12 +0100

    feat: update Button

--- src/app/shared/ui/button/button.component.spec.ts
+++ src/app/shared/ui/button/button.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
 
 describe('ButtonComponent', () => {
-  let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
+  let component: ButtonComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent]
-    })
-    .compileComponents();
+      imports: [ButtonComponent], // standalone component!
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('renders projected content', () => {
+    fixture.nativeElement.innerHTML = `<app-button>Click me</app-button>`;
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button?.textContent).toContain('Click me');
+  });
+
+  it('emits event on click', () => {
+    spyOn(component.onClick, 'emit');
+
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click');
+    expect(component.onClick.emit).toHaveBeenCalled();
   });
 });
--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -8,11 +8,19 @@ import { ChangeDetectionStrategy, Component, EventEmitter, input, Output } from 
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // TODO: Learn how to use inputs
+  // inputs: [
+  //   'variant$$:variant',
+  //   'icon$$:icon',
+  // ]
 })
 export class ButtonComponent {
-  readonly variant$$ = input<'primary' | 'secondary' | 'link'>('primary');
+  readonly variant$$ = input<'primary' | 'secondary' | 'link'>('secondary');
   readonly loading$$ = input(false);
   readonly disabled$$ = input(false);
+  readonly icon$$ = input<string | null>(null);
+  readonly fullWidth$$ = input(false);
+  readonly type$$ = input<'button' | 'submit' | 'reset'>('button');
 
-  @Output() pressed = new EventEmitter<void>();
+  @Output() onClick = new EventEmitter<void>();
 }
